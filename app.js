b7112f108d6ffe550fda1f89214d0639b372fcb4
kaeix <rodrigoviclima@gmail.com>
2023-06-18 22:19:02 -0300

    a

---
+++ app.js
@@ -0,0 +1,11 @@
+document.getElementById('botao-confirmacao').addEventListener('click', function() {
+    // Exibir o áudio
+    var audio = document.getElementById('audio');
+    audio.style.display = 'block';
+
+    // Exibir o vídeo
+    var video = document.getElementById('video1');
+    video.style.display = 'block';
+    var video1 = document.getElementById('video2');
+    video1.style.display = 'block';
+  });
